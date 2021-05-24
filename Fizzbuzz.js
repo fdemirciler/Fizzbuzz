17b013beac7a1830efc921d0c2343dc95e0384ba
fdemirciler <fdemirciler@gmail.com>
2021-05-25 01:40:01 +0300

    Javascriptt exexercise on solving Fizzbuzz puzzle. This is a part of The Odin Project.

---
+++ Fizzbuzz.js
@@ -0,0 +1,26 @@
+function result() {
+    const input = document.getElementById('input').value;
+    const number = parseInt(input);
+    const print = document.getElementById('output');
+
+    for (let i = 1; i <= number; i++) {
+
+        if (i % 3 === 0 && i % 5 === 0) {
+
+            print.innerHTML += 'FizzBuzz <br/>'
+
+        } else if (i % 3 === 0) {
+
+            print.innerHTML += 'Fizz <br/>'
+
+        } else if (i % 5 === 0) {
+
+            print.innerHTML += 'Buzz <br/>'
+
+        } else {
+
+            print.innerHTML += i + ' <br/>'
+        }
+
+    }
+}
