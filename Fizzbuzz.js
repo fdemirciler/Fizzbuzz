9842a8c05959087ccf7379cf5839b6a4eaf3f5fe
fdemirciler <fdemirciler@gmail.com>
2021-05-28 22:37:41 +0300

    JS and CSS updated

--- Fizzbuzz.js
+++ Fizzbuzz.js
@@ -10,26 +10,29 @@ function result() {
 
     const input = document.getElementById('input').value;
     const number = parseInt(input);
-    const print = document.getElementById('output');
+    const print = document.getElementById('output').getElementsByTagName('tbody')[0];
+
 
     for (let i = 1; i <= number; i++) {
 
+
         if (i % 3 === 0 && i % 5 === 0) {
 
-            print.innerHTML += 'FizzBuzz <br/>'
+            print.insertRow().insertCell().innerHTML = 'FizzBuzz'
 
         } else if (i % 3 === 0) {
 
-            print.innerHTML += 'Fizz <br/>'
+            print.insertRow().insertCell().innerHTML = 'Fizz'
 
         } else if (i % 5 === 0) {
 
-            print.innerHTML += 'Buzz <br/>'
+            print.insertRow().insertCell().innerHTML = 'Buzz'
 
         } else {
 
-            print.innerHTML += i + ' <br/>'
+            print.insertRow().insertCell().innerHTML = i
         }
 
     }
+
 }
